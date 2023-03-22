24cb1385d9228f8c0abbcb9430b2d555b401f863
Egor-oop <gulidoegor@gmail.com>
2023-03-22 08:23:05 +0300

    eltex & cisco backup script

--- router_move/backup.py
+++ router_move/backup.py
@@ -1,10 +1,14 @@
 import socket
 from .backup_mikrotik import backup_mikrotik
+from .backup_eltex import backup_eltex
+from .backup_cisco import backup_cisco
 
 
 def run_backup(directory: str) -> None:
     try:
         backup_mikrotik(directory)
+        backup_eltex(directory)
+        backup_cisco(directory)
     except socket.gaierror:
         print('unknown ip')
 
